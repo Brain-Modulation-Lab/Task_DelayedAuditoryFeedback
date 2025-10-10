9bb70e6299466db4dbc480fdeb4964e2f66bc37f
samhansen29 <samkhansen29@gmail.com>
2025-10-10 16:23:12 -0400

    improved intraop, cleaning up folders

--- Task_SentenceOnsetElectricalStimulation-main/Archive/m_008_generate_random_table_stimulation.m
+++
@@ -1,7 +0,0 @@
-close all
-clear all
-clc
-%% read the stimualtion table
-
-number_of_blocks=4;
-total_blocks = get_randomized_stim_trials("Stimcond.csv",number_of_blocks );
--- Task_SentenceOnsetElectricalStimulation-main/README.md
+++
@@ -1,2 +0,0 @@
-# Task_SentenceOnsetElectricalStimulation
-Implementation of intra-operative stimulation task from the DBS leads. 
