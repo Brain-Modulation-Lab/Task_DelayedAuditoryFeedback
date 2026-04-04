41b6351a752007218f7471504680173689cf6726
amsmeier <amsmeier@users.noreply.github.com>
2026-04-04 14:19:35 -0400

    changed so that START_DAF_MIDI always records via focusrite channels (even for preop)

--- START_DAF_MIDI.m
+++ START_DAF_MIDI.m
@@ -227,19 +227,11 @@ end
 cd(scriptPath);
 
 %%% record audio via parallel pool function
-if strcmp (cfg.SESSION_LABEL,'preop')
-    if ~(exist('record_audio_preop','file')==2)
-        clear onCleanupTasks
-        error('record_audio_preop() not found on path.');
-    end
-    future = parfeval(@record_audio_preop, 1, cfg.AUDIO_FILENAME, workerQueueConstant);
-    future.Diary;
-    onCleanupTasks{6} = onCleanup(@() send(workerQueueClient, 'stop'));
-elseif strcmp (cfg.SESSION_LABEL,'intraop')
-    future = parfeval(@record_audio, 1, cfg.AUDIO_FILENAME, workerQueueConstant);
-    future.Diary;
-    onCleanupTasks{6} = onCleanup(@() send(workerQueueClient, 'stop'));
-end
+% we could add an option to call 'record_audio_laptop_only' instead of 'record_audio' if focusrite is not detected
+future = parfeval(@record_audio, 1, cfg.AUDIO_FILENAME, workerQueueConstant);
+future.Diary;
+onCleanupTasks{6} = onCleanup(@() send(workerQueueClient, 'stop'));
+
 
 %% Ripple neurophysiology hardware communication setup (optional)
 cfg.DIGOUT = 0;
